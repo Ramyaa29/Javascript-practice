5ffe72d9e2db5749fccb8151f1f1024b321af676
Ramyaa29 <ramyageesala2003@gmail.com>
2025-10-04 15:21:25 +0530

    Numbers

--- index.js
+++ index.js
@@ -1,29 +1,18 @@
-// let x = 'Hello World';
-// console.log(x);
+let x;
 
-let x = new String('hello world');
+const num= new Number(5);
+// x= num.toString();5
+// x= num.toString().length;1
+// x=num.toFixed(1); output: 5.0
+// x= num.toPrecision(3); 5.00
+// x= num.toExponential(2);
 
-// s= x.toUpperCase();
-// s = x.toLowerCase();
-// s = x.charAt(2);
-// s = x.indexOf('d');
-// s= x. substring(2,7);
+// x=num.toLocaleString('en -IN');
+// x=num.valueOf();5
+// x= Number.MAX_VALUE;
+// x= Number.MIN_VALUE;
 
-// Substring and slice is same but slice can be indexed in negative
-
-// s = x.slice(-10,-6);
-
-// x = '    Hello World   '
-// s= x.trim();
-
-// s= x.replace('world', 'Ramya');
-
-// s=x.includes('hell');
-
-// s = x.split(' ');  output: ['hello', 'world']
-
-// s = x.split(''); output: ['h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd']
-console.log(s);
+console.log(x);
 
 
 
