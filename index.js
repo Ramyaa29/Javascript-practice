babccc91ccbeb064ddd2a2b7ebff9817aed5af1d
Ramyaa29 <ramyageesala2003@gmail.com>
2025-10-04 13:23:42 +0530

    Javascript

---
+++ index.js
@@ -0,0 +1,28 @@
+// let x = 'Hello World';
+// console.log(x);
+
+let x = new String('hello world');
+
+// s= x.toUpperCase();
+// s = x.toLowerCase();
+// s = x.charAt(2);
+// s = x.indexOf('d');
+// s= x. substring(2,7);
+
+// Substring and slice is same but slice can be indexed in negative
+
+// s = x.slice(-10,-6);
+
+// x = '    Hello World   '
+// s= x.trim();
+
+// s= x.replace('world', 'Ramya');
+
+// s=x.includes('hell');
+
+// s = x.split(' ');  output: ['hello', 'world']
+
+// s = x.split(''); output: ['h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd']
+console.log(s);
+
+
